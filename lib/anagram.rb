e0d714befd3af2b9208aa6efdb8dd31a5686cddc
Andrew Fairbairn <asfairbairn4@gmail.com>
2022-12-13 21:42:41 -0600

    commit tree c71ef4c093675bc636f8750559f89243d483e6df

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match array
+        letters = @word.chars
+        array_letters = array.filter {|x| x.chars}
+        letters.sort == array_letters.sort
+    end
+end
